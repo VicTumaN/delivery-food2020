12fa913c79d2266ed9e277b78eb7b9313d0cf72b
VicTumaN <63468692+VicTumaN@users.noreply.github.com>
2020-04-10 21:22:00 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const CartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
+
+CartButton.addEventListener('click', toggleModal)
+close.addEventListener('click', toggleModal)
+
+function toggleModal() {
+    modal.classList.toggle("is-open");
+}
+
+new WOW().init();
